e44ca540897f526c5f33e301d5cfda9672e83dc8
Koki Fukuda <ko.fu.dev@gmail.com>
2022-05-14 01:08:30 +0900

    Implement setf

--- std/std.lisp
+++ std/std.lisp
@@ -14,3 +14,41 @@
          ,@body
          (setq _list-head (cdr _list-head)))
        ,result)))
+
+(defun caar (cons)
+  (car (car cons)))
+
+(defun cdar (cons)
+  (cdr (car cons)))
+
+(defun cadar (cons)
+  (car (cdr (car cons))))
+
+(defmacro setf (&rest vals)
+  (let ((result '(progn)))
+    (let ((tail result))
+      (while vals
+        (if (< (length vals) 2)
+            (error "vals must be multiple of 2"))
+        (if (symbolp (car vals))
+            (progn
+              (setcdr tail `((setq ,(car vals) ,(nth 1 vals))))
+              (setq tail (cdr tail))))
+        (if (consp (car vals))
+            (progn
+              (setcdr tail
+                      (cond
+                        ((eq (caar vals) 'car)
+                         `((setcar ,(cadar vals) ,(nth 1 vals))))
+                        ((eq (caar vals) 'cdr)
+                         `((setcdr ,(cadar vals) ,(nth 1 vals))))
+                        ((eq (caar vals) 'car)
+                         `((setcar ,(cadar vals) ,(nth 1 vals))))
+                        ((eq (caar vals) 'nth)
+                         `((setcar (nthcdr ,(cadar vals) ,(nth 1 (cdar vals))) ,(nth 1 vals))))
+                        (t (error "Unsupported target of setf"))))
+              (setq tail (cdr tail))))
+        (if (consp (car vals))
+            ())
+        (setq vals (cdr (cdr vals)))))
+    result))
